31713bc7b0f086d2be8feda7c1d5a45657921e46
arvind-s <arvind.s01994@gmail.com>
2015-01-15 18:36:33 +0530

    With image transfer

--- RTSP/src/MakeVideo/CreateImages.java
+++ RTSP/src/MakeVideo/CreateImages.java
@@ -13,46 +13,42 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 /**
- *
+ * 
  * @author arvind
  */
-public class CreateImages 
-{
-    Robot robo;
-    
-    private BufferedImage capture(int name)
-    {
-        BufferedImage image = null;
-        try
-        {
-            robo = new Robot();
-            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-            Rectangle scrnRect = new Rectangle(dim);
-            image = robo.createScreenCapture(scrnRect);
-            
-            //File img = new File("images/"+name+".jpg");
-            //ImageIO.write(image, "jpg", img);
-        }
-        catch(AWTException e)
-        {
-            e.printStackTrace();
-        } 
-        /*catch (IOException ex) 
-        {
-            Logger.getLogger(CreateVideo.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
-        return image;
-    }
-    protected ArrayList<BufferedImage> captureMultiple(int start,int number) throws InterruptedException
-    {
-        ArrayList<BufferedImage> imagesList = new ArrayList<BufferedImage>();
-        for(int i=start;i<number;i++)
-        {
-            BufferedImage image = this.capture(i);
-            imagesList.add(image);
-            Thread.sleep(180);
-        }
-        return imagesList;
-    }
-    
+public class CreateImages {
+	Robot robo;
+
+	private BufferedImage capture(int name) {
+		BufferedImage image = null;
+		try {
+			robo = new Robot();
+			Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+			Rectangle scrnRect = new Rectangle(dim);
+			image = robo.createScreenCapture(scrnRect);
+
+			// File img = new File("images/"+name+".jpg");
+			// ImageIO.write(image, "jpg", img);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+		/*
+		 * catch (IOException ex) {
+		 * Logger.getLogger(CreateVideo.class.getName()).log(Level.SEVERE, null,
+		 * ex); }
+		 */
+		return image;
+	}
+
+	protected ArrayList<BufferedImage> captureMultiple(int start, int number)
+			throws InterruptedException {
+		ArrayList<BufferedImage> imagesList = new ArrayList<BufferedImage>();
+		for (int i = start; i < number; i++) {
+			BufferedImage image = this.capture(i);
+			imagesList.add(image);
+			Thread.sleep(180);
+		}
+		return imagesList;
+	}
+
 }
