43580bf08212f2f048536c02e9a3f6c6c4891edd
arvind-s <arvind.s01994@gmail.com>
2015-03-12 19:51:02 +0530

    redirecting python output

--- Python Stuff/RunPython.java
+++ Python Stuff/RunPython.java
@@ -1,9 +1,18 @@
+import java.io.*;
 public class RunPython
 {
 	public static void main(String args[])throws Exception
 	{
+		StringBuffer output = new StringBuffer();
 		Runtime rt = Runtime.getRuntime();
 		Process pr = rt.exec("python python.py");
-		System.out.println("Done");
+		pr.waitFor();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+		String line = "";			
+		while ((line = reader.readLine())!= null) 
+		{
+			output.append(line + "\n");
+		}
+		System.out.println(output.toString());
 	}
 }
