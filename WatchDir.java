666ed72b857449c8a61b82983cc60ab25c1c22f2
Akash Agarwal <akash10agarwal@gmail.com>
2015-04-19 13:02:53 +0530

    POST Request & some other modifications

--- WatchDir.java
+++ WatchDir.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
+import java.io.ObjectInputStream.GetField;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.file.FileSystem;
 import java.nio.file.*;
 import static java.nio.file.StandardWatchEventKinds.*;
@@ -27,9 +29,7 @@ public class WatchDir {
    private final Map<WatchKey,Path> keys;
    private final boolean recursive;
    private boolean trace = false;//?
-   
-
-   
+      
    @SuppressWarnings("unchecked")
    static <T> WatchEvent<T> cast(WatchEvent<?> event) {
       return (WatchEvent<T>)event;
@@ -133,26 +133,47 @@ public class WatchDir {
             //System.out.println(child.getClass().getSimpleName());
             
             String y=child.toString();
- 
             
-            if(kind==ENTRY_MODIFY)//?
+ 
+            /*ENTRY_MODIFY->
+             * 				updation of d contents->
+             * 				just renaming->
+             * if old & new absolute paths r d same,den updation=>send d new free size,new file size,absolute path & ip
+             * else just send d absolute path of d file renamed & d ip
+             */
+            
+            if(kind==ENTRY_MODIFY)
             {
+                
                File file = new File(y);
                
                if (!file.exists() || !file.isFile()) 
                   System.out.println("File doesn\'t exist");
-               else//?
                
-               
+               else
                {
+                   //File file1 = new File("c:");//?
+                   //System.out.println("Filesize in bytes: " + file1.length());//?
+            	   long fl=file.length();
+            	   String fl1=Long.toString(fl);
+             	  try 
+             	  {
+						postAbsolutePath(file.getAbsolutePath(),fl1,"updatesize");
+						postFreeAvailableSize("update");						
+				  }
+             	  catch (Exception e) 
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+             	  
              /*
+             
                new Thread(new Runnable() 
                {
                  public void run() 
                  {
                
-                  File file = new File("c:");//?
-                  System.out.println("Filesize in bytes: " + file.length());//?
                   
                   long usableSpace = file.getUsableSpace(); ///unallocated / free disk space in bytes.
                   
@@ -200,11 +221,46 @@ public class WatchDir {
                }).start();
                   */
                }
+            }
+
+               /*ENTRY_DELETE->Absolute path of d file which got deleted alongwith d IP Address as usual which is obvio
+                Also send d total free size now available*/
+               
+            else if(kind==ENTRY_DELETE)
+               {
+                   
+                  File file = new File(y);
+                  
+                  if (!file.exists() || !file.isFile())//? 
+                     System.out.println("File doesn\'t exist");
+                  else//?
+                  
+                  {
+               	   //System.out.println("testing:"+file.getAbsolutePath());
+                	  try 
+                	  {
+                		  postAbsolutePath(file.getAbsolutePath(),"0","delete");
+                		  
+                		  //postAbsolutePath(file.getAbsolutePath(),"0","updatesize");
+     
+						postFreeAvailableSize("update");
+						
+					}
+                	  catch (Exception e) 
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+                	  
+                  }
+               }
                         
             // if directory is created, and watching recursively, then
             // register it and its sub-directories
             
-               if (recursive && (kind == ENTRY_CREATE)) {
+               if (recursive && (kind == ENTRY_CREATE))//work left 
+               {
+            	   
                   try {
                      if (Files.isDirectory(child, NOFOLLOW_LINKS)) {
                         registerAll(child);
@@ -230,11 +286,117 @@ public class WatchDir {
             }
          }
       }
+public static void postAbsolutePath(String ap,String s,String u) throws Exception//File?
+{
+	String url=" http://IP_ADDRESS:3000/files/"+u;
+	   
+    @SuppressWarnings("deprecation")//?
+    HttpClient client = new DefaultHttpClient();
+    
+    HttpPost post = new HttpPost(url);
+     
+     List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+     
+      String ipaddr=InetAddress.getLocalHost().getHostAddress();
+      //String fp=getTotalFreeAvailableSize();
+      
+     		urlParameters.add(new BasicNameValuePair("ip",ipaddr));
+     		//urlParameters.add(new BasicNameValuePair("freesize",fp));
+     		urlParameters.add(new BasicNameValuePair("absolutepath",ap));
+     		urlParameters.add(new BasicNameValuePair("size",s));
+     
+     post.setEntity(new UrlEncodedFormEntity(urlParameters));
+     
+     HttpResponse response = client.execute(post);
+     System.out.println("\nSending 'POST' request to URL : " + url);
+     System.out.println("Post parameters : " + post.getEntity());
+     System.out.println("Response Code : " + 
+                          response.getStatusLine().getStatusCode());
+     
+     BufferedReader rd = new BufferedReader(
+              new InputStreamReader(response.getEntity().getContent()));
+     
+     StringBuffer result = new StringBuffer();
+     String line = "";
+     while ((line = rd.readLine()) != null) 
+     {
+     result.append(line);
+     }
+     
+     System.out.println(result.toString());
+
+}
+   public static void postFreeAvailableSize(String u) throws Exception
+   {
+	   String url="http://IP_ADDRESS:3000/device/"+u;
+	   
+       //@SuppressWarnings("deprecation")//?
+       HttpClient client = new DefaultHttpClient();
+       
+       HttpPost post = new HttpPost(url);
+        
+        List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+        
+         String ipaddr=InetAddress.getLocalHost().getHostAddress();
+         String fp=getTotalFreeAvailableSize();
+         
+        		urlParameters.add(new BasicNameValuePair("ip",ipaddr));
+        		urlParameters.add(new BasicNameValuePair("freesize",fp));
+        
+        post.setEntity(new UrlEncodedFormEntity(urlParameters));
+        
+        HttpResponse response = client.execute(post);
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + post.getEntity());
+        System.out.println("Response Code : " + 
+                             response.getStatusLine().getStatusCode());
+        
+        BufferedReader rd = new BufferedReader(
+                 new InputStreamReader(response.getEntity().getContent()));
+        
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) 
+        {
+        result.append(line);
+        }
+        
+        System.out.println(result.toString());
+
    }
-   public static void main(String[] args) throws IOException// 
+   public static String getTotalFreeAvailableSize()
    {
 	      long total=0,usableSpace;
 	      
+		  File[] drives = File.listRoots();
+		  
+	      if (drives != null && drives.length > 0) 
+	      {
+	         for (File aDrive : drives)
+	         {  
+	         
+	            final String x;
+	            x=aDrive.toString().substring(0,2);//
+	            System.out.println(x);
+	            
+	            File file = new File(x);//?
+	            
+	      //      System.out.println("Filesize in bytes: " + file.length());
+	            
+	           usableSpace = file.getUsableSpace(); ///unallocated / free disk space in bytes.
+	            
+	           total+=usableSpace;//?
+	         }
+	      }
+	      total/=1024;//in KBs
+	   return Long.toString(total);
+	   
+   }
+   public static void main(String[] args) throws IOException,Exception// 
+   {
+	      
+	  postFreeAvailableSize("insert");
+	   
 	  File[] drives = File.listRoots();
 	  
       if (drives != null && drives.length > 0) 
@@ -245,15 +407,7 @@ public class WatchDir {
             final String x;
             x=aDrive.toString().substring(0,2);//
             System.out.println(x);
-            
-            File file = new File(x);//?
-            
-      //      System.out.println("Filesize in bytes: " + file.length());
-            
-           usableSpace = file.getUsableSpace(); ///unallocated / free disk space in bytes.
-            
-           total+=usableSpace;//?
-            
+               
             // register directory and process its events           
             new Thread(
                   new Runnable() 
@@ -299,49 +453,11 @@ public class WatchDir {
          {
            public void run() 
            {*/
-         
-         System.out.println(total/1024);//in KBs
-         
-        String url = "http://10.11.113.53:3000/device/insert";//
-         
-        @SuppressWarnings("deprecation")//
-         
-        HttpClient client = new DefaultHttpClient();//
-        HttpPost post = new HttpPost(url);
-         
-         List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
-         
-          String ipaddr=InetAddress.getLocalHost().getHostAddress();
-          
-          String fp=Long.toString(total/1024);// in KBs
-          
-         		urlParameters.add(new BasicNameValuePair("ip",ipaddr));
-         		urlParameters.add(new BasicNameValuePair("freesize",fp));
-         
-         post.setEntity(new UrlEncodedFormEntity(urlParameters));
-         
-         HttpResponse response = client.execute(post);
-         System.out.println("\nSending 'POST' request to URL : " + url);
-         System.out.println("Post parameters : " + post.getEntity());
-         System.out.println("Response Code : " + 
-                              response.getStatusLine().getStatusCode());
-         
-         BufferedReader rd = new BufferedReader(
-                  new InputStreamReader(response.getEntity().getContent()));
-         
-         StringBuffer result = new StringBuffer();
-         String line = "";
-         while ((line = rd.readLine()) != null) 
-         {
-         result.append(line);
-         }
-         
-         System.out.println(result.toString());
-         
+                  
           /*}         
          
          }).start();*/
 
-      }//
+      }
    }
 }
