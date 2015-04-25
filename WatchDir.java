f02be616a79683565f457f4ec3bc8e5015054bbc
Akash Agarwal <akash10agarwal@gmail.com>
2015-04-25 14:05:02 +0530

    Final documented

--- WatchDir.java
+++ WatchDir.java
@@ -1,11 +1,49 @@
+/***************************************************************************
+*
+*   SOFTWARE ENGINEERING PROJECT - 12CS354 - VI SEM BE (PESIT)
+*
+*
+*       NETWORK STORAGE - SE PROJECT TEAM 1
+*
+*
+*       JOB     - HOOKS
+*
+*
+*       AUTHORS - SURAJ PAI
+*               - AKASH AGARWAL
+*
+*
+*       TASK    - To recursively register a "watch" on certain directories 
+*                 as a whole ,present on any given system, connected onto the 
+*                 same common network as others,looking for any kind of 
+*                 changes in the file systems,and POST(reflect) the same back to 
+*                 the tracker server for the corresponding updations required 
+*                 in it,for any future references.
+*                 
+*                
+*       START   - February 13th
+*
+****************************************************************************/
+
+package hooks;
+
+/***************************************************************************
+*
+*   The following statements are used to import the various modules
+*   which are required for the prooject.
+*
+****************************************************************************/
+
 import java.io.IOException;
 import java.io.ObjectInputStream.GetField;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.file.FileSystem;
 import java.nio.file.*;
+
 import static java.nio.file.StandardWatchEventKinds.*;
 import static java.nio.file.LinkOption.*;
+
 import java.nio.file.attribute.*;
 import java.io.*;
 import java.security.Timestamp;
@@ -20,47 +58,58 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import constants.Constants;
+
 /**
- * Example to watch a directory (or tree) for changes to files.
+ * Code to watch a directory (or tree) for changes to files.
  */
  
-public class WatchDir {
-
+public class WatchDir
+ {
    private final WatchService watcher;
    private final Map<WatchKey,Path> keys;
    private final boolean recursive;
-   private boolean trace = false;//?
+   private boolean trace = false;
       
    @SuppressWarnings("unchecked")
-   static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+   static <T> WatchEvent<T> cast(WatchEvent<?> event) 
+   {
       return (WatchEvent<T>)event;
    }
 
    /**
     * Register the given directory with the WatchService
     */
-   private void register(Path dir) throws IOException {
+	
+   private void register(Path dir) throws IOException 
+   {
       WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
-      if (trace) {
-         Path prev = keys.get(key);//?
-         if (prev == null) {
-            System.out.format("register: %s\n", dir);//?
-         } 
-         else {
-            if (!dir.equals(prev)) {
-               System.out.format("update: %s -> %s\n", prev, dir);//?
-            }
-         }
+	  
+      if (trace) 
+	  {
+         Path prev = keys.get(key);
+		 
+         if (prev == null) 
+            System.out.format("register: %s\n", dir);
+         else
+			 {
+            if (!dir.equals(prev))
+               System.out.format("update: %s -> %s\n", prev, dir);
+			 }
       }
-      keys.put(key, dir);//?
+      keys.put(key, dir);
    }
 
    /**
     * Register the given directory, and all its sub-directories, with the
     * WatchService.
     */
-   private void registerAll(final Path start) throws IOException {
+	
+	
+   private void registerAll(final Path start) throws IOException 
+   {
       // register directory and sub-directories
+	  
       Files.walkFileTree(start,
             new SimpleFileVisitor<Path>() 
             {
@@ -76,54 +125,58 @@ public class WatchDir {
    /**
     * Creates a WatchService and registers the given directory
     */
-   WatchDir(Path dir, boolean recursive) throws IOException {
+	
+   WatchDir(Path dir, boolean recursive) throws IOException 
+   {
       this.watcher = FileSystems.getDefault().newWatchService();
       this.keys = new HashMap<WatchKey,Path>();
       this.recursive = recursive;
    
-      if (recursive) {
+      if (recursive) 
+	  {
          System.out.format("Scanning %s ...\n", dir);
          registerAll(dir);
          System.out.println("Scanning "+dir+" Done.");
       } 
-      else {
+      else 
          register(dir);
-      }
-   
+ 
       // enable trace after initial registration
-      this.trace = true;//?
+      this.trace = true;
    }
 
    /**
     * Process all events for keys queued to the watcher
     */
-   void processEvents() {
-      for (;;) {
+	
+   void processEvents() 
+   {
+      for (;;) //Infinite loop
+	  {
       
          // wait for key to be signalled
          WatchKey key;
-         try {
+         try 
+		 {
             key = watcher.take();
          } 
-         catch (InterruptedException x) {
+         catch (InterruptedException x) 
+		 {
             return;
          }
       
          Path dir = keys.get(key);
-         if (dir == null) {
-            System.err.println("WatchKey not recognized!!");//?
+         if (dir == null) 
+		 {
+            System.err.println("WatchKey not recognized!!");
             continue;
          }
       
-         for (WatchEvent<?> event: key.pollEvents()) {
+         for (WatchEvent<?> event: key.pollEvents()) 
+		 {
             WatchEvent.Kind kind = event.kind();
          
-            // TBD - provide example of how OVERFLOW event is handled
-            if (kind == OVERFLOW) {
-               continue;
-            }
-         
-            // Context for directory entry event is the file name of entry????
+            // Context for directory entry event is the file name of entry
             WatchEvent<Path> ev = cast(event);
             Path name = ev.context();
             Path child = dir.resolve(name);
@@ -139,8 +192,8 @@ public class WatchDir {
             /*ENTRY_MODIFY->
              * 				updation of d contents->
              * 				just renaming->
-             * if old & new absolute paths r d same,den updation=>send d new free size,new file size,absolute path & ip
-             * else just send d absolute path of d file renamed & d ip
+             * If old & new absolute paths are same,then updation=>send the new free size,new file size,absolute path & ip
+             *  or else just send the absolute path of the file renamed.
              */
             
             if(kind==ENTRY_MODIFY)
@@ -153,8 +206,6 @@ public class WatchDir {
                
                else
                {
-                   //File file1 = new File("c:");//?
-                   //System.out.println("Filesize in bytes: " + file1.length());//?
             	   long fl=file.length();
             	   String fl1=Long.toString(fl);
              	  try 
@@ -167,65 +218,11 @@ public class WatchDir {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-             	  
-             /*
-             
-               new Thread(new Runnable() 
-               {
-                 public void run() 
-                 {
-               
-                  
-                  long usableSpace = file.getUsableSpace(); ///unallocated / free disk space in bytes.
-                  
-                  //long freeSpace = file.getFreeSpace(); //unallocated / free disk space in bytes.
-                 
-                  //System.out.println("Space free : " + usableSpace + " bytes");
-                  //System.out.println("Space free : " + freeSpace + " bytes");
-       
-                  
-                  
-               String url = "http://10.11.113.53:3000/device/insert";
-               
-               HttpClient client = new DefaultHttpClient();
-               HttpPost post = new HttpPost(url);
-               
-               List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
-               
-                String ipaddr=InetAddress.getLocalHost().getHostAddress();
-                String fp=usablespace.toString();
-                
-               		urlParameters.add(new BasicNameValuePair("ip",ipaddr));
-               		urlParameters.add(new BasicNameValuePair("freesize",fp));
-               
-               post.setEntity(new UrlEncodedFormEntity(urlParameters));
-               
-               HttpResponse response = client.execute(post);
-               System.out.println("\nSending 'POST' request to URL : " + url);
-               System.out.println("Post parameters : " + post.getEntity());
-               System.out.println("Response Code : " + 
-                                    response.getStatusLine().getStatusCode());
-               
-               BufferedReader rd = new BufferedReader(
-                        new InputStreamReader(response.getEntity().getContent()));
-               
-               StringBuffer result = new StringBuffer();
-               String line = "";
-               while ((line = rd.readLine()) != null) {
-               result.append(line);
-               }
-               
-               System.out.println(result.toString());
-               
-               }
-               
-               }).start();
-                  */
                }
             }
 
-               /*ENTRY_DELETE->Absolute path of d file which got deleted alongwith d IP Address as usual which is obvio
-                Also send d total free size now available*/
+               /*ENTRY_DELETE->Absolute path of the file which got deleted ,alongwith the IP Address.
+                Also send the total free size now available*/
                
             else if(kind==ENTRY_DELETE)
                {
@@ -234,16 +231,11 @@ public class WatchDir {
                   
                   if (!file.exists() || !file.isFile())//? 
                      System.out.println("File doesn\'t exist");
-                  else//?
-                  
+                  else
                   {
-               	   //System.out.println("testing:"+file.getAbsolutePath());
                 	  try 
                 	  {
                 		  postAbsolutePath(file.getAbsolutePath(),"0","delete");
-                		  
-                		  //postAbsolutePath(file.getAbsolutePath(),"0","updatesize");
-     
 						postFreeAvailableSize("update");
 						
 					}
@@ -262,7 +254,8 @@ public class WatchDir {
                if (recursive && (kind == ENTRY_CREATE)) 
                {
             	   
-                  try {
+                  try 
+				  {
                      if (Files.isDirectory(child, NOFOLLOW_LINKS)) 
                      {
                         registerAll(child);
@@ -271,7 +264,7 @@ public class WatchDir {
 
                      File file = new File(y);
                      
-                     if (!file.exists() || !file.isFile())//? 
+                     if (!file.exists() || !file.isFile())
                         System.out.println("File doesn\'t exist");
                      else
                      {
@@ -287,22 +280,23 @@ public class WatchDir {
                          
                     	 postAbsolutePath(file.getAbsolutePath(),c,"insert");
                      }
-                  	  
-                     
                   } 
-                  catch (Exception x) {
+                  catch (Exception x) 
+				  {
                   // ignore to keep sample readable
                   }
                }
             }
          
-         // reset key and remove from set if directory no longer accessible??
+         // reset key and remove from set if directory no longer accessible
+		 
             boolean valid = key.reset();
             if (!valid) 
             {
                keys.remove(key);
             
             // all directories are inaccessible
+			
                if (keys.isEmpty()) 
                {
                   break;
@@ -310,11 +304,13 @@ public class WatchDir {
             }
          }
       }
-public static void postAbsolutePath(String ap,String s,String u) throws Exception//File?
+	  
+public static void postAbsolutePath(String ap,String s,String u) throws Exception
 {
 	String url=" http://IP_ADDRESS:3000/files/"+u;
-	   
-    @SuppressWarnings("deprecation")//?
+	System.out.println("URL:http://"+Constants.myIp+":3000/files/");
+	
+    @SuppressWarnings("deprecation")
     HttpClient client = new DefaultHttpClient();
     
     HttpPost post = new HttpPost(url);
@@ -409,25 +405,23 @@ public static void postAbsolutePath(String ap,String s,String u) throws Exceptio
 	         {  
 	         
 	            final String x;
-	            x=aDrive.toString().substring(0,2);//
+	            x=aDrive.toString().substring(0,2);
 	            System.out.println(x);
 	            
-	            File file = new File(x);//?
-	            
-	      //      System.out.println("Filesize in bytes: " + file.length());
+	            File file = new File(x);
 	            
 	           usableSpace = file.getUsableSpace(); ///unallocated / free disk space in bytes.
 	            
-	           total+=usableSpace;//?
+	           total+=usableSpace;
 	         }
 	      }
 	      total/=1024;//in KBs
 	   return Long.toString(total);
 	   
    }
-   public static void main(String[] args) throws IOException,Exception// 
+   public static void startHooks() throws IOException,Exception
    {
-	      
+	  System.out.println("Started hooks service...");
 	  postFreeAvailableSize("insert");
 	   
 	  File[] drives = File.listRoots();
@@ -438,7 +432,7 @@ public static void postAbsolutePath(String ap,String s,String u) throws Exceptio
          {  
          
             final String x;
-            x=aDrive.toString().substring(0,2);//
+            x=aDrive.toString().substring(0,2);
             System.out.println(x);
                
             // register directory and process its events           
@@ -454,25 +448,8 @@ public static void postAbsolutePath(String ap,String s,String u) throws Exceptio
                         }
                         catch (AccessDeniedException e) 
                         {
-                           try //?
-                           {
-                              Thread.sleep(2);
-                           } 
-                           catch (InterruptedException e1) 
-                           {
-                           // TODO Auto-generated catch block
-                              e1.printStackTrace();
-                           }
-                           Path dir = Paths.get(x);
-                           try 
-                           {
-                              new WatchDir(dir, true).processEvents();
-                           } 
-                           catch (IOException e1) 
-                           {
-                           // TODO Auto-generated catch block
-                              e1.printStackTrace();
-                           }//?
+                              e.printStackTrace();
+							  System.out.println(e.getMessage());
                         }
                         catch (Exception e) 
                         {
@@ -482,15 +459,6 @@ public static void postAbsolutePath(String ap,String s,String u) throws Exceptio
                      }
              }).start();
          }
-         /*new Thread(new Runnable() 
-         {
-           public void run() 
-           {*/
-                  
-          /*}         
-         
-         }).start();*/
-
       }
    }
 }
