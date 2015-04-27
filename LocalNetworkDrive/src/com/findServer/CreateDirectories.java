b5ec54dfff66e09e6a956a1238a8307fae3dbf4c
arvind-s <arvind.s01994@gmail.com>
2015-04-27 09:33:22 +0530

    Daemon create directories

---
+++ LocalNetworkDrive/src/com/findServer/CreateDirectories.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.findServer;
+
+import constants.Constants;
+import java.io.File;
+
+/**
+ *
+ * @author arvind
+ */
+public class CreateDirectories 
+{
+    public static void createDirectories()
+    {
+        String dirName = Constants.subnetId;
+        for(int i=0;i<255;i++)
+        {
+            dirName+=i;
+            File dir = new File(dirName);
+            if(!dir.exists())
+            {
+                System.out.println("Dir creation:"+dirName+",Creation success"+dir.mkdirs());
+            }
+        }
+    }
+}
